0315f7d30b2b6f58a33d6140956e2b815ae5e629
rooch001 <dwivediroochin@gmail.com>
2020-03-27 13:55:48 +0530

    Modal Account and add foriegn key

--- Bug_tracker_app/bug_tracker-project/accounts/models.py
+++ Bug_tracker_app/bug_tracker-project/accounts/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-    
+class Account(models.Model):
+    employeeId = models.AutoField(primary_key=True)
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    age = models.IntegerField(verbose_name="age")
+    mobile = models.CharField(max_length=13)
+    jobTitle = models.CharField(max_length=15) 
+    email = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.firstName + " " + self.lastName
