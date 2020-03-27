87f2226ffa591bb638eeb47923b559724894680f
Himanshu Nagar <himanshunagar323@gmail.com>
2020-03-27 17:08:17 +0530

    Account Model Created

--- Bug_tracker_app/bug_tracker-project/accounts/models.py
+++ Bug_tracker_app/bug_tracker-project/accounts/models.py
@@ -2,13 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Account(models.Model):
-    employeeId = models.AutoField(primary_key=True)
-    firstName = models.CharField(max_length=30)
-    lastName = models.CharField(max_length=30)
-    age = models.IntegerField(verbose_name="age")
-    mobile = models.CharField(max_length=13)
-    jobTitle = models.CharField(max_length=15) 
-    email = models.ForeignKey(User, on_delete=models.CASCADE)
+    emp_id      = models.AutoField(primary_key=True)
+    first_name  = models.CharField(max_length=30)
+    last_name   = models.CharField(max_length=30)
+    age         = models.IntegerField()
+    mobile      = models.CharField(max_length=13)
+    job_title   = models.CharField(max_length=15)
+    email       = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo       = models.ImageField(upload_to='images/photos/')
+    address     = models.TextField(max_length=100)
 
     def __str__(self):
-        return self.firstName + " " + self.lastName
+        return self.first_name + " " + self.last_name
