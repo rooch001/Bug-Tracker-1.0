d27dd2ba4f48fa4640710a901abfd5d50916088d
rooch001 <dwivediroochin@gmail.com>
2020-03-28 21:08:51 +0530

    done bhaddi chiz

--- Bug_tracker_app/bug_tracker-project/dashboard/models.py
+++ Bug_tracker_app/bug_tracker-project/dashboard/models.py
@@ -9,8 +9,8 @@ class Project(models.Model):
     project_id = models.AutoField(primary_key=True)
     project_name = models.CharField(max_length=30)
     description = models.TextField()
-    project_file = models.FileField(upload_to='files/project_file/')
-    additional_file = models.FileField(upload_to='files/additional_file/')
+    project_file = models.FileField(upload_to='files/project_file/',null=True)
+    additional_file = models.FileField(upload_to='files/additional_file/',null=True)
     date_of_initiation = models.DateField()
     deadline = models.DateField()
 
--- Bug_tracker_app/bug_tracker-project/dashboard/views.py
+++ Bug_tracker_app/bug_tracker-project/dashboard/views.py
@@ -7,39 +7,26 @@ from dashboard.models import Project
 def home(request):
     if request.user.is_authenticated:
         current_user = request.user
-<<<<<<< HEAD
         try:
             account = get_object_or_404(Account, email=current_user)
             return render(request, 'dashboard/home.html', {'account': account})
         except:
             auth.logout(request)
             return render(request, 'accounts/login.html', {'error': 'Your profile details are not ready'})
-=======
-        account = get_object_or_404(Account, email=current_user)
-        return render(request, 'dashboard/home.html', {'account':account})
->>>>>>> 86075ae4e5687e75beaa7dec9776be6dbf374cef
     else:
         return redirect('login')
 
 def addproject(request):
     if request.method == "POST":
         project = Project()
-        if request.POST["project_name"] and request.POST["editor1"] or request.POST["inputGroupFile01"] and request.POST["inputGroupFile02"] and request.POST["start_date"] and request.POST["dead_line"]:
 
-            project.project_name = request.POST["project_name"]
-            project.description = request.POST["editor1"]
-            project.project_file = request.POST["inputGroupFile01"]
-            project.additional_file = request.POST["inputGroupFile02"]
-
-            startdate = request.POST['start_date']
-            formatedStartDate = startdate.strftime("%Y-%m-%d")
-            project.date_of_initiation = formatedStartDate
-
-            deadLine = request.POST['start_date']
-            formatedDeadLine = deadLine.strftime("%Y-%m-%d")
-
-            project.deadline = formatedDeadLine
-            project.save()
+        project.project_name = request.POST["project_name"]
+        project.description = request.POST["editor1"]
+        project.project_file = request.FILES["input01"]
+        project.additional_file = request.FILES["input02"]
+        project.date_of_initiation = request.POST['start_date']
+        project.deadline = request.POST['dead_line']
+        project.save()
         return render(request, 'dashboard/addproject.html')
     else:
         current_user = request.user
